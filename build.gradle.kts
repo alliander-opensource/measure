2734c78a491e526c5aa757046a455e90d1dad4ad
Geert Mulders <gmulders@gmail.com>
2024-03-07 14:59:18 +0100

    Upgrade configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.5.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.9.23" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,13 +11,12 @@
  * in the user manual at https://docs.gradle.org/6.7.1/userguide/multi_project_builds.html
  */
 
-rootProject.name = "Measure"
+rootProject.name = "measure"
 include("measure")
 include("measure-generator")
 
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        jcenter()
     }
 }
